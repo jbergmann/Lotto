92a5dbbaac3988dd9d3559d083993873e0cd4fe9
Jürgen Bergmann (Wien) <jbergmann@juergen-bergmann.at>
2020-03-03 09:23:45 +0100

    commit

--- Lotto/src/at/juergenbergmann/lotto/Lotto.java
+++ Lotto/src/at/juergenbergmann/lotto/Lotto.java
@@ -1,24 +1,30 @@
 package at.juergenbergmann.lotto;
 
-import java.util.HashSet;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Random;
 
 public class Lotto {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		HashSet<Integer> gezogen = new HashSet<>();
+		//HashSet<Integer> gezogen = new HashSet<>();
+		List<Integer> gezogen = new ArrayList<>();
 		Random rnd = new Random();
 		Integer zahl;
 		
 		for(Integer i=0; i<12; i++) {
 			for(Integer j=0; j<6; j++) {
-				do {
+				zahl = rnd.nextInt(45) + 1;
+				
+				while(!gezogen.add(zahl))
+				{
 					zahl = rnd.nextInt(45) + 1;
 				}
-				while(!gezogen.add(zahl));
 			}
 			
+			Collections.sort(gezogen);
 			for(Integer k : gezogen) {
 				System.out.print(k + " ");
 			}
---
+++ Lotto/src/at/juergenbergmann/lotto/Ziehung.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package at.juergenbergmann.lotto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author juerg
+ *
+ */
+public class Ziehung {
+	List<Integer> gezogen = new ArrayList<Integer>();
+}
